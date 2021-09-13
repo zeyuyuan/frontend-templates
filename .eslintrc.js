0c809388f0eb79e108866f17c22653e7b6af0e78
yuanzeyu <yuanzeyu@onchain.com>
2021-09-13 11:44:14 +0800

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,38 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking", // It will slow down the lint performance
+    "plugin:promise/recommended",
     "plugin:prettier/recommended",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 12,
+    project: ["./tsconfig.eslint.json"],
     parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: [".vue"],
   },
   plugins: ["vue", "@typescript-eslint", "prettier"],
   rules: {
     "prettier/prettier": "error",
+    "import/prefer-default-export": "off", // export default is bad
+    "@typescript-eslint/unbound-method": "off", // for useI18n
+    "vue/html-button-has-type": "error",
+    "vue/component-name-in-template-casing": [
+      "error",
+      "PascalCase",
+      {
+        registeredComponentsOnly: false,
+      },
+    ],
+    "vue/custom-event-name-casing": ["error", "camelCase"],
+    "vue/valid-define-props": "error",
+    "vue/valid-define-emits": "error",
+    "vue/v-for-delimiter-style": "error",
   },
   globals: {
     defineProps: "readonly",
