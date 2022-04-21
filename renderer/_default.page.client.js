f1d0562e8b815f9f1d9d5d6b37aec37c82eba192
zeyuyuan <yzy@pcomg.com>
2022-04-21 15:41:41 +0800

    chore: update dependencies

--- renderer/_default.page.client.js
+++ renderer/_default.page.client.js
@@ -1,5 +1,8 @@
 import { getPage } from 'vite-plugin-ssr/client'
 import { createApp } from './app'
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/typography.css'
 
 const hydrate = async () => {
   // We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
